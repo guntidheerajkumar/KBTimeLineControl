c8a1cf39ff83550d4a5b87dd8d36d694ab449fd8
Dheeraj Kumar G <guntidheerajkumar@gmail.com>
2016-11-17 13:23:37 +0530

    KBTimeLineControl
    Awesome way of representing the time line and the current status.

---
+++ KBTimeLineControl/ApiDefinition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CoreGraphics;
+using Foundation;
+using UIKit;
+
+namespace KBTimeLineControl
+{
+	// @interface TimeLineViewControl : UIView
+	[BaseType(typeof(UIView))]
+	interface TimeLineViewControl
+	{
+		// @property (assign, nonatomic) CGFloat viewheight;
+		[Export("viewheight")]
+		nfloat Viewheight { get; set; }
+
+		// -(id)initWithTimeArray:(NSArray *)time andTimeDescriptionArray:(NSArray *)timeDescriptions andCurrentStatus:(int)status andFrame:(CGRect)frame;
+		[Export("initWithTimeArray:andTimeDescriptionArray:andCurrentStatus:andFrame:")]
+		IntPtr Constructor(NSObject[] time, NSObject[] timeDescriptions, int status, CGRect frame);
+	}
+}
---
+++ SampleApp/ViewController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using KBTimeLineControl;
+using UIKit;
+using Foundation;
+using CoreGraphics;
+
+namespace SampleApp
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			var weeks = new NSObject[] {
+				new NSString("Sunday"),
+				new NSString("Monday"),
+				new NSString("Tuesday"),
+				new NSString("Wednesday"),
+				new NSString("Thursday"),
+				new NSString("Friday"),
+				new NSString("Saturday"),
+			};
+
+			var descriptions = new NSObject[] {
+				new NSString("Buy Groceries to Home"),
+				new NSString("Need to Submit Bills"),
+				new NSString("Pay Mobile Bill"),
+				new NSString("Pay Electricity Bill"),
+				new NSString("Book tickets for Vacation."),
+				new NSString("Plan for weekend."),
+				new NSString("Buy a gift for the party."),
+			};
+
+			TimeLineViewControl control = new TimeLineViewControl(weeks,
+																  descriptions,
+																  3,
+																  new CGRect(30, 30,
+																			 this.View.Frame.Width - 60, this.View.Frame.Height - 40));
+
+			this.View.AddSubview(control);
+		}
+	}
+}
